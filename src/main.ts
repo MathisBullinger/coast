847c153046c09e355f97fa20c3ae309a1f55ea00
Mathis Bullinger <mathis@bullinger.dev>
2022-09-25 16:27:52 +0200

    svg zooming

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+const svg = document.querySelector('svg')!
+
+let viewBox: ViewPort = { x: 0, y: 0, w: 0, h: 0 }
+
+const applyViewBox = () => {
+  svg.setAttribute(
+    'viewBox',
+    `${viewBox.x} ${viewBox.y} ${viewBox.w} ${viewBox.h}`
+  )
+}
+
+{
+  const minSize = 1000
+  viewBox.w =
+    (svg.clientWidth / Math.min(svg.clientWidth, svg.clientHeight)) * minSize
+  viewBox.h =
+    (svg.clientHeight / Math.min(svg.clientWidth, svg.clientHeight)) * minSize
+  viewBox.x = (minSize - viewBox.w) / 2
+  viewBox.y = (minSize - viewBox.h) / 2
+
+  applyViewBox()
+}
+
+const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+path.setAttribute('d', `M 0 500 l 1000 0`)
+path.setAttribute('stroke', '#000')
+path.setAttribute('vector-effect', 'non-scaling-stroke')
+svg.appendChild(path)
+
+type ViewPort = { x: number; y: number; w: number; h: number }
+type Point = [x: number, y: number]
+type Path = Point[]
+
+svg.addEventListener('wheel', (e) => {
+  e.preventDefault()
+  if (!e.ctrlKey) return
+
+  const x = e.clientX / svg.clientWidth
+  const y = e.clientY / svg.clientHeight
+  const m = 1 + e.deltaY / 500
+
+  const newViewBox = { ...viewBox }
+  newViewBox.w *= m
+  newViewBox.h *= m
+  newViewBox.x += (viewBox.w - newViewBox.w) * x
+  newViewBox.y += (viewBox.h - newViewBox.h) * y
+
+  viewBox = newViewBox
+  applyViewBox()
+})
